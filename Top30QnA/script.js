142f70d4df09b7093f78578bbc398ba6057d80e8
Pavan R Teli <143809171+pavanrteli@users.noreply.github.com>
2026-08-05 21:07:08 +0530

    Add links for useCallback and useMemo explanations

--- Top30QnA/script.js
+++ Top30QnA/script.js
@@ -424,6 +424,7 @@
 //         Use useMemo when you want to optimize and cache the result of a computation that depends on certain inputs.
 //         Use useCallback when you want to optimize the creation of callback functions that are passed to child components.
 // useCallBack - https://codesandbox.io/p/sandbox/6wdn6l?file=%2Fsrc%2FApp.js%3A9%2C2
+// useMemo - https://codesandbox.io/p/sandbox/ds55td?file=%2Fsrc%2FComponents%2FUseMemo%2Fusememo.jsx%3A19%2C16
 
 
 // ------------------------------------------23. What is useRef---------------------------------------------
